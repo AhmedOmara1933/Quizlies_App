8081a8b626853a4649086519ed55edb4b0ada4dc
ahmedomara <ahmedhfmo@gmail.com>
2023-10-03 19:06:52 +0300

    commit

--- lib/modules/2.quiz_levels.dart
+++ lib/modules/2.quiz_levels.dart
@@ -17,44 +17,24 @@ class QuzzielesLevel extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-
       ),
-      body:Padding(
-        padding: const EdgeInsets.only(top:40.0),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 40.0),
         child: GridView(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-          mainAxisSpacing: 40.0,
+            crossAxisCount: 2,
+            mainAxisSpacing: 40.0,
           ),
-
           children: [
-            DefaultLevel(
-                number:'01',
-                levelcolor:const Color(0xff7a5cfb)
-            ),
-            DefaultLevel(
-                number:'02',
-                levelcolor:const Color(0xff287ffc)
-            ),
-            DefaultLevel(
-                number:'03',
-                levelcolor:const Color(0xfffb652a)
-            ),
-            DefaultLevel(
-                number:'04',
-                levelcolor:const Color(0xff14b6a8)
-            ),
-            DefaultLevel(
-                number:'05',
-                levelcolor:const Color(0xfff12490)
-            ),
-            DefaultLevel(
-                number:'06',
-                levelcolor:const Color(0xff3f2c7f)
-            ),
+            DefaultLevel(number: '01', levelcolor: const Color(0xff7a5cfb)),
+            DefaultLevel(number: '02', levelcolor: const Color(0xff287ffc)),
+            DefaultLevel(number: '03', levelcolor: const Color(0xfffb652a)),
+            DefaultLevel(number: '04', levelcolor: const Color(0xff14b6a8)),
+            DefaultLevel(number: '05', levelcolor: const Color(0xfff12490)),
+            DefaultLevel(number: '06', levelcolor: const Color(0xff3f2c7f)),
           ],
         ),
       ),
-      );
+    );
   }
 }
